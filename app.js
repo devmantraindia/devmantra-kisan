2a4a2df4f55eb854aa84ecec892590a2598889ef
Dev Mantra <insurance.akashworldwide@gmail.com>
2026-05-31 17:36:22 +0530

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 import { CreateWebWorkerMLCEngine } from "https://esm.run/@mlc-ai/web-llm";
 
+const chatBox = document.querySelector(".chat-msgs");
+const input = document.getElementById("userInput");
+const sendBtn = document.getElementById("sendBtn");
+
+sendBtn.innerText = "⏳";
+
 const engine = await CreateWebWorkerMLCEngine(
   new Worker("./worker.js", { type: "module" }),
   "Qwen2.5-1.5B-Instruct-q4f16_1"
 );
 
-window.askAI = async function (message) {
+sendBtn.innerText = "➤";
+
+sendBtn.addEventListener("click", async () => {
+  const question = input.value.trim();
+  if (!question) return;
+
+  chatBox.innerHTML += `
+  <div class="msg-u">
+    <div class="mav">👤</div>
+    <div class="mbbl">${question}</div>
+  </div>
+  `;
+
+  input.value = "";
+
   const response = await engine.chat.completions.create({
     messages: [
       {
@@ -14,10 +34,17 @@ window.askAI = async function (message) {
       },
       {
         role: "user",
-        content: message
+        content: question
       }
     ]
   });
 
-  return response.choices[0].message.content;
-};
+  chatBox.innerHTML += `
+  <div class="msg-ai">
+    <div class="mav">🌾</div>
+    <div class="mbbl">
+      ${response.choices[0].message.content}
+    </div>
+  </div>
+  `;
+});
